72548a988d2d697cd7fe94183ba30621968949ee
MattW <mattnwarnock@yahoo.co.uk>
2023-09-13 18:36:54 +0100

    Lambda to recognise clip query

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 module.exports.handler = async (event) => {
+  let message = "I don't know what type of clip you want!";
+
+  if (event.queryStringParameters) {
+    message = `I understand the request for a ${event.queryStringParameters.feel} type of clip`;
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0! Your function executed successfully!",
+        message: message,
         input: event,
       },
       null,

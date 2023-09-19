c9c8f22cc114c3121e6f5766b5ebd736c97993b2
MattW <mattnwarnock@yahoo.co.uk>
2023-09-19 18:24:08 +0100

    Script to spawn child event for newman command

--- scripts/output.js
+++ scripts/output.js
@@ -1,7 +1,27 @@
-function handler (data, serverless, options) {
-	
-  console.log('endpoint', data.HttpApiUrl)
-	
+const { spawn } = require("child_process");
+
+function handler(cfdata, serverless, options) {
+  const globalVar = `endpoint=${cfdata.HttpApiUrl}`
+
+  const newman = spawn("newman run fixtures/clipgift-endpoint-tests.postman_collection.json", [`--global-var ${globalVar}`]);
+
+  newman.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  newman.stderr.on("data", (data) => {
+    console.log(`stderr: ${data}`);
+  });
+
+  newman.on("error", (error) => {
+    console.log(`error: ${error.message}`);
+  });
+
+  newman.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+
+  console.log("endpoint", cfdata.HttpApiUrl);
 }
-	
-module.exports = { handler }
+
+module.exports = { handler };

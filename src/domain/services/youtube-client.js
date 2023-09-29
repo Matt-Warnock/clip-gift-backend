c03a8859ca8f2c4d2b4fb1ad12e1f6997a10136f
MattW <mattnwarnock@yahoo.co.uk>
2023-09-29 19:39:22 +0100

    Refactor test format

--- src/domain/services/youtube-client.js
+++ src/domain/services/youtube-client.js
@@ -7,18 +7,19 @@ class YoutubeClient {
 
   constructor() {
     this.#searchEndpoint = `${process.env.YOUTUBE_END_POINT}/search`;
-    this.#aPIKey = process.env.YOUTUBE_API_KEY; 
+    this.#aPIKey = process.env.YOUTUBE_API_KEY;
   }
 
   async searchVideo(searchString) {
     const queries = {
-      part: 'snippet',
+      part: "snippet",
       maxResults: 1,
       q: searchString,
-      type: 'video',
-      videoDuration: 'short',
+      type: "video",
+      videoDuration: "short",
       key: this.#aPIKey,
     };
+
 
     const response = await axios.get(this.#searchEndpoint, { params: queries });
   }
--- src/domain/services/youtube-client.test.js
+++ src/domain/services/youtube-client.test.js
@@ -2,26 +2,22 @@ const YoutubeClient = require("./youtube-client");
 const nock = require("nock");
 
 describe("youtube client", () => {
-  const endpoint = process.env.YOUTUBE_END_POINT;
-  const aPIKey = process.env.YOUTUBE_API_KEY;
-
-  it("calls youtube endpoint with passed search string & queries", async () => {
-    const searchString = 'kittens';
-    const queries = {
-      part: 'snippet',
+  const client = new YoutubeClient(),
+    endpoint = process.env.YOUTUBE_END_POINT,
+    aPIKey = process.env.YOUTUBE_API_KEY,
+    searchString = "kittens",
+    queries = {
+      part: "snippet",
       maxResults: 1,
       q: searchString,
-      type: 'video',
-      videoDuration: 'short',
+      type: "video",
+      videoDuration: "short",
       key: aPIKey,
     };
 
-    const scope = nock(endpoint)
-      .get('/search')
-      .query(queries)
-      .reply(200);
+  it("calls youtube endpoint with passed search string & queries", async () => {
+    const scope = nock(endpoint).get("/search").query(queries).reply(200);
 
-    const client = new YoutubeClient();
     await client.searchVideo(searchString);
 
     expect(scope.isDone()).toBe(true);

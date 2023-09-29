fbd92c0069bc276ec3d333ec43d07d59695df555
MattW <mattnwarnock@yahoo.co.uk>
2023-09-29 16:55:40 +0100

    Test Endpoint with search queries

--- src/domain/services/youtube-client.js
+++ src/domain/services/youtube-client.js
@@ -5,8 +5,10 @@ require('dotenv').config();
 class YoutubeClient {
     async request () {
         const youtube_endpoint = process.env.YOUTUBE_END_POINT;
+        const APIKey = process.env.YOUTUBE_API_KEY;
+        const kittenSearchQueries = 'part=snippet&maxResults=1&q=kittens&type=video&videoDuration=short'
 
-        const response = await axios.get(`${youtube_endpoint}/`)
+        const response = await axios.get(`${youtube_endpoint}/search?${kittenSearchQueries}&key=${APIKey}`)
     };
 };
 
--- src/domain/services/youtube-client.test.js
+++ src/domain/services/youtube-client.test.js
@@ -2,9 +2,23 @@ const YoutubeClient = require('./youtube-client');
 const nock = require('nock');
 
 describe('youtube client', () => {
-    it('calls to the youtube endpoint', async () => {
+    it('calls to the youtube endpoint with kitten search queries', async () => {
         const endpoint = process.env.YOUTUBE_END_POINT
-        const scope = nock(endpoint).get('/').reply(200);
+        const APIKey = process.env.YOUTUBE_API_KEY
+        const kittenSearchQueries = {
+            part: 'snippet',
+            maxResults: 1,
+            q: 'kittens',
+            type: 'video',
+            videoDuration: 'short',
+            key: APIKey
+        }
+
+        const scope = nock(endpoint)
+        .get('/search')
+        .query(kittenSearchQueries)
+        .reply(200);
+
 
         const client = new YoutubeClient;
         await client.request();
